e4432e4924f33f1237f8d717f4c06c08398b002d
Jason Begleiter <jhbegs204@gmail.com>
2018-05-29 17:05:21 +0300

    Working end to end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'A moonrocket in Berlin',
+  address: 'Soaring through the stars, Berlin, Germany',
+  description: 'A crazy freaking rocket ship that shoots you off into space at 11Gs and has all the Tang you could ever want',
+  price_per_night: 100,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Giant egg in Verona',
+  address: '2 stores down by the crazy cat lady, Verona, Italy',
+  description: 'What else could you want but a human-sized egg to stay in?',
+  price_per_night: 250,
+  number_of_guests: 5
+)
